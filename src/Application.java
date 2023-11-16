7a1a0e852781838c53bc292ac241da8fc26b131b
Banuran <58660859+Banuran@users.noreply.github.com>
2023-11-16 10:36:42 +0100

    refactor and repeat measurement multiple times

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,58 @@
+import Runnables.Factorization;
+
 import java.time.Duration;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         int numThreads = 100000;
+
+        measureTime();
+//        long elapsedTime = startThreads(numThreads, false);
+//        System.out.println("\n\nPlatform Threads:");
+//        String formattedElapsedTime = getReadableElapsedTime(elapsedTime);
+//        System.out.println(formattedElapsedTime);
+//
+//        elapsedTime = startThreads(numThreads, true);
+//        System.out.println("\n\nVirtual Threads:");
+//        formattedElapsedTime = getReadableElapsedTime(elapsedTime);
+//        System.out.println(formattedElapsedTime);
+    }
+
+    private static void measureTime() throws InterruptedException {
+        int numOfRepeats = 20;
+        int[] numsThreads = {6, 50, 100, 1000};
+
+        for (int numThreads : numsThreads) {
+            long totalTime = 0;
+            for (int i = 0; i < numOfRepeats; i++) {
+                totalTime += startThreads(numThreads, false);
+            }
+            long avgTime = totalTime / numOfRepeats;
+            String formattedAvgTime = getReadableElapsedTime(avgTime);
+            System.out.println(numThreads + " platform avg: " + formattedAvgTime);
+
+             totalTime = 0;
+            for (int i = 0; i < numOfRepeats; i++) {
+                totalTime += startThreads(numThreads, true);
+            }
+            avgTime = totalTime / numOfRepeats;
+            formattedAvgTime = getReadableElapsedTime(avgTime);
+            System.out.println(numThreads + " virtual avg: " + formattedAvgTime);
+        }
+    }
+
+    private static long startThreads(int numThreads, boolean virtual) throws InterruptedException {
         Thread[] threads = new Thread[numThreads];
 
         long start = System.nanoTime();
 
 
         for (int i = 0; i < numThreads; i++) {
-            Thread thread = Thread.ofPlatform().start(new Factorization());
+            Thread thread;
+            if (virtual)
+                thread = Thread.ofVirtual().start(new Factorization());
+            else
+                thread = Thread.ofPlatform().start(new Factorization());
             threads[i] = thread;
         }
 
@@ -18,51 +61,14 @@ public class Application {
         }
 
         long end = System.nanoTime();
-        System.out.println("\n\nPlatform Threads:");
-        String formattedElapsedTime = getReadableElapsedTime(start, end);
-        System.out.println(formattedElapsedTime);
 
-        start = System.nanoTime();
-
-        for (int i = 0; i < numThreads; i++) {
-            Thread thread = Thread.ofVirtual().start(new Factorization());
-            threads[i] = thread;
-        }
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        end = System.nanoTime();
-        System.out.println("\n\nVirtual Threads:");
-        formattedElapsedTime = getReadableElapsedTime(start, end);
-        System.out.println(formattedElapsedTime);
+        return end-start;
     }
 
-    public static String getReadableElapsedTime(long start, long end) {
-        Duration duration = Duration.ofNanos(end - start);
+    private static String getReadableElapsedTime(long elapsedTime) {
+        Duration duration = Duration.ofNanos(elapsedTime);
         return String.format("%02d m %02d s %d ms %d ns",
                 duration.toMinutesPart(), duration.toSecondsPart(),
                 duration.toMillisPart(), duration.toNanosPart() % 1000000L);
-    }
-}
-
-class Factorization implements Runnable {
-    @Override
-    public void run() {
-        // https://www.katharinengymnasium.de/wolf/web/javaIDE/primfaktorenJavaIDE.html
-        int zahl = 87654321; // integer bis 2 147 483 647
-        int teiler = 2;
-        int teilerMax = (int) Math.sqrt(zahl);
-        while (teiler <= teilerMax) {
-            if(zahl % teiler == 0) {
-                zahl /= teiler;
-                teilerMax = (int) Math.sqrt(zahl);
-                //System.out.print("Z\b");
-            }
-            else {
-                teiler++;
-            }
-        }
     }
 }
---
+++ src/Runnables/Factorization.java
@@ -0,0 +1,22 @@
+package Runnables;
+
+public class Factorization implements Runnable {
+    @Override
+    public void run() {
+        // https://www.katharinengymnasium.de/wolf/web/javaIDE/primfaktorenJavaIDE.html
+        int zahl = 87654321; // integer bis 2 147 483 647
+        int teiler = 2;
+        int teilerMax = (int) Math.sqrt(zahl);
+        while (teiler <= teilerMax) {
+            if(zahl % teiler == 0) {
+                zahl /= teiler;
+                teilerMax = (int) Math.sqrt(zahl);
+                //System.out.print("Z\b");
+            }
+            else {
+                teiler++;
+            }
+        }
+    }
+}
+
