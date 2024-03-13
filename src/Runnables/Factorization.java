7382ce1c2b7af8f305c04db0a6f4cbdf800bb4dc
PhiJoKo <58660859+PhiJoKo@users.noreply.github.com>
2024-03-13 20:37:19 +0100

    add option to change number for Factorization Runnable

--- src/Runnables/Factorization.java
+++ src/Runnables/Factorization.java
@@ -1,10 +1,19 @@
 package Runnables;
 
 public class Factorization implements Runnable {
+
+    private int zahl = 87654321;
+
+    public Factorization() {
+    }
+    public Factorization(int zahl) {
+        this.zahl = zahl;
+    }
+
     @Override
     public void run() {
         // https://www.katharinengymnasium.de/wolf/web/javaIDE/primfaktorenJavaIDE.html
-        int zahl = 87654321; // integer bis 2 147 483 647
+        int zahl = this.zahl; // integer bis 2 147 483 647
         int teiler = 2;
         int teilerMax = (int) Math.sqrt(zahl);
         while (teiler <= teilerMax) {
