7e263097e80c330ea907cf638a2dd6c2b4df3239
PhiJoKo <58660859+PhiJoKo@users.noreply.github.com>
2024-03-13 20:32:57 +0100

    add Runnable with semaphore

---
+++ src/Runnables/BlockingSemaphore.java
@@ -0,0 +1,28 @@
+package Runnables;
+
+import java.util.concurrent.Semaphore;
+
+public class BlockingSemaphore implements Runnable {
+    private final Semaphore semaphore;
+    private final Runnable runCalc;
+
+    public BlockingSemaphore(Semaphore semaphore) {
+        this.semaphore = semaphore;
+        this.runCalc = new Factorization(19111680);
+    }
+
+    @Override
+    public void run() {
+
+        this.runCalc.run();
+
+        try {
+            semaphore.acquire();
+            this.runCalc.run();
+            semaphore.release();
+        } catch (InterruptedException e) {
+            System.out.println("Failed acquiring Semaphore.");
+            throw new RuntimeException(e);
+        }
+    }
+}
