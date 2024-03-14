fb1eff2ef2fcd26db1b5bee5ea6f816be7f1a0d4
PhiJoKo <58660859+PhiJoKo@users.noreply.github.com>
2024-03-14 19:22:46 +0100

    add runnableType to config

--- src/PerformanceMeasurementConfig.java
+++ src/PerformanceMeasurementConfig.java
@@ -4,6 +4,7 @@ public class PerformanceMeasurementConfig {
 
     private int numRepeats = 10;
     private int[] numsThreads = {100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,409600};
+    private RunnableType runnableType = RunnableType.FACTORIZATION;
 
     public int getNumRepeats() {
         return numRepeats;
@@ -11,6 +12,10 @@ public class PerformanceMeasurementConfig {
 
     public int[] getNumsThreads() {
         return numsThreads;
+    }
+
+    public RunnableType getRunnableType() {
+        return runnableType;
     }
 
     public void setNumRepeats(int numRepeats) {
@@ -21,8 +26,12 @@ public class PerformanceMeasurementConfig {
         this.numsThreads = numsThreads;
     }
 
+    public void setRunnableType(RunnableType runnableType) {
+        this.runnableType = runnableType;
+    }
+
     public String toString() {
-        return String.format("Number of Threads to test: %s%nNumber of repeats per test: %d",
-                Arrays.toString(numsThreads), numRepeats);
+        return String.format("Number of Threads to test: %s%nNumber of repeats per test: %d%nRunnable to run: %s",
+                Arrays.toString(numsThreads), numRepeats, runnableType.toString());
     }
 }
